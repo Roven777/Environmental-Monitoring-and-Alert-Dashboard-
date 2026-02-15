4a0a3e00963dae8aeb5807a3eeb6abb4f1813c82
Roven777 <kroven2994@gmail.com>
2026-02-15 14:23:47 +0530

    backend

--- backend/src/config/socket.js
+++ backend/src/config/socket.js
@@ -5,7 +5,7 @@ const handleAlertSocket = require("../socketa/alert.socket");
 module.exports = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: "*",
       methods: ["GET", "POST"],
     },
   });
